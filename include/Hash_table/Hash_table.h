2a9bd3a5e560a6286a1617e1609901487b14e184
Darktezi <darktezi@mail.ru>
2024-05-07 16:54:11 +0400

    create class hashTable and basic functions

---
+++ include/Hash_table/Hash_table.h
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+template<typename KeyType, typename ValueType>
+class HashTable {
+private:
+    struct HashNode {
+        KeyType key;
+        ValueType value;
+        HashNode(KeyType k, ValueType v) : key(k), value(v) {}
+    };
+
+    size_t size;
+    std::vector<std::list<HashNode>> table;
+
+    size_t hash(const KeyType& key) const {
+        size_t hashValue = 0;
+        for (const auto& ch : key) {
+            hashValue += ch;
+        }
+        return hashValue % size;
+    }
+
+public:
+    HashTable(size_t tableSize) : size(tableSize), table(tableSize) {}
+    HashTable(const HashTable& other) : size(other.size), table(other.table) {}
+    ~HashTable() = default;
+
+    HashTable& operator=(const HashTable& other) {
+        if (this != &other) {
+            size = other.size;
+            table = other.table;
+        }
+        return *this;
+    }
+};
