2898dac95ee4e342dc87c31d131d6fda747fec90
Darktezi <darktezi@mail.ru>
2024-05-07 17:42:32 +0400

    add other methods

--- include/Hash_table/Hash_table.h
+++ include/Hash_table/Hash_table.h
@@ -2,25 +2,24 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <cmath>
 
-template<typename KeyType, typename ValueType>
+template<typename K, typename T>
 class HashTable {
 private:
     struct HashNode {
-        KeyType key;
-        ValueType value;
-        HashNode(KeyType k, ValueType v) : key(k), value(v) {}
+        K key;
+        T value;
+        HashNode(K k, T v) : key(k), value(v) {}
     };
 
     size_t size;
     std::vector<std::list<HashNode>> table;
+    const double A = 0.6180339887;
 
-    size_t hash(const KeyType& key) const {
-        size_t hashValue = 0;
-        for (const auto& ch : key) {
-            hashValue += ch;
-        }
-        return hashValue % size;
+    size_t hash(const K& key) const {
+        size_t hashValue = static_cast<size_t>(size * (std::fmod(key * A, 1)));
+        return hashValue;
     }
 
 public:
@@ -34,5 +33,82 @@ public:
             table = other.table;
         }
         return *this;
+    }
+
+    void print() {
+        for (size_t i = 0; i < size; ++i) {
+            std::cout << "Bucket " << i << ": ";
+            for (const auto& node : table[i]) {
+                std::cout << "(" << node.key << ", " << node.value << ") ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void insert(const K& key, const T& value) {
+        size_t index = hash(key);
+        for (auto& node : table[index]) {
+            if (node.key == key) {
+                node.value = value;
+                return;
+            }
+        }
+        table[index].emplace_back(key, value);
+    }
+
+    void insert_or_assign(const K& key, const T& value) {
+        size_t index = hash(key);
+        for (auto& node : table[index]) {
+            if (node.key == key) {
+                node.value = value;
+                return;
+            }
+        }
+        table[index].emplace_back(key, value);
+    }
+
+    bool contains(const T& value) const {
+        for (const auto& bucket : table) {
+            for (const auto& node : bucket) {
+                if (node.value == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    T* search(const K& key) {
+        size_t index = hash(key);
+        for (auto& node : table[index]) {
+            if (node.key == key) {
+                return &node.value;
+            }
+        }
+        return nullptr;
+    }
+
+    bool erase(const K& key) {
+        size_t index = hash(key);
+        auto& bucket = table[index];
+        auto it = std::find_if(bucket.begin(), bucket.end(), [&](const HashNode& node) {
+            return node.key == key;
+            });
+        if (it != bucket.end()) {
+            bucket.erase(it);
+            return true;
+        }
+        return false;
+    }
+
+    int count(const K& key) const {
+        size_t index = hash(key);
+        int count = 0;
+        for (const auto& node : table[index]) {
+            if (node.key == key) {
+                ++count;
+            }
+        }
+        return count;
     }
 };
